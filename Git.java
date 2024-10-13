9aaa2dcc9b6610966cd644308b427c784121bb54
Edwin Luhnow <eluhnow1@hwemail.com>
2024-10-13 12:09:04 -0700

    Part 3 100% complete (#3)
    * Testing if forked repo correctly
    
    * Finished 1st 3 milestones of Part 3
    
    * finished interface implementation
    
    * Added checkout and updated readme
    
    * Added combatibility for staging edits

--- Git.java
+++ Git.java
@@ -3,20 +3,27 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import java.util.zip.DeflaterOutputStream;
 import java.nio.channels.*;;
-
-public class Git{
+public class Git implements GitInterface{
     //if compressFiles is true, git will zip files before caching them
     public static final boolean COMPRESS_FILES = false;
     
+    //test commit
     public Git (){
         initializeRepository();
     }
@@ -26,8 +33,9 @@ public class Git{
         File git = new File("./git/");
         File objects = new File("./git/objects/");
         File index = new File ("./git/index");
+        File head = new File("./git/HEAD");
         //Checks if repository currently exists
-        if (git.exists() && objects.exists() && index.exists())
+        if (git.exists() && objects.exists() && index.exists() && head.exists())
             System.out.println ("Git Repository already exists");
         else{
             if (!git.exists())
@@ -42,12 +50,21 @@ public class Git{
                     e.printStackTrace();
                 }
             }
+            if (!head.exists()) {
+                try {
+                    head.createNewFile();}
+                catch (IOException e){
+                    System.out.println ("Head creation failed");
+                    e.printStackTrace();
+                }
+            }
+            
         }
     }
 
     //string filePath is part of blob to be hashed
     //Uses HashFile for files
-    public void createBlobGeneral(String filePath) throws IOException{
+    public void stage(String filePath) throws IOException{
         File file = new File(filePath);
         String filename = file.getName();
         //checks if blob exits
@@ -65,7 +82,7 @@ public class Git{
             FileChannel destination = null;
             File[] allFiles = file.listFiles();
             for (File child : allFiles){
-                createBlobGeneral(child.getPath());
+                stage(child.getPath());
             }
             File file_combined = new File("./combined" + file.getName());
             for (File child : allFiles){
@@ -94,26 +111,29 @@ public class Git{
                     destination.close();
                 }
             }
-            
-            // write to index
-            try{
-                //Checks if filename and hash is already in index
-                boolean isInIndex = false;
-                String index = ("tree " + hashCode + " " + file.getPath());
-                BufferedReader reader = new BufferedReader(new FileReader("./git/index"));
-                while (reader.ready()){
-                    if (index.equals(reader.readLine()))
-                        isInIndex = true;
-                }
-                reader.close();
-                if (!isInIndex){
-                    BufferedWriter writer = new BufferedWriter(new FileWriter("./git/index", true));
-                    writer.write(index);
-                    writer.newLine();
-                    writer.close();
+
+            //checks if entry exists already and deletes if so
+            List<String> indexLines = new ArrayList<>();
+            File indexFile = new File("./git/index");
+            try (BufferedReader reader = new BufferedReader(new FileReader(indexFile))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split(" ");
+                    if (!parts[2].equals(file.getPath())) {
+                        indexLines.add(line);
+                    }
                 }
             }
-            catch (IOException e){
+            
+            //write to index
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(indexFile))) {
+                for (String indexLine : indexLines) {
+                    writer.write(indexLine);
+                    writer.newLine();
+                }
+                writer.write("tree " + hashCode + " " + file.getPath());
+                writer.newLine();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
@@ -121,7 +141,7 @@ public class Git{
 
     //string filePath is path of blob to be hashed
     //returns true if blob hashes, returns false otherwise
-    public void HashFile (File file, String filename){
+    public void HashFile (File file, String filename) throws FileNotFoundException, IOException{
         //compresses the file
         if (COMPRESS_FILES)
             file = compress(file);
@@ -147,25 +167,27 @@ public class Git{
             }
         }
 
-        // write to index
-        try{
-            //Checks if filename and hash is already in index
-            boolean isInIndex = false;
-            String index = ("blob " + hashCode + " " + file.getPath());
-            BufferedReader reader = new BufferedReader(new FileReader("./git/index"));
-            while (reader.ready()){
-                if (index.equals(reader.readLine()))
-                    isInIndex = true;
-            }
-            reader.close();
-            if (!isInIndex){
-                BufferedWriter writer = new BufferedWriter(new FileWriter("./git/index", true));
-                writer.write(index);
-                writer.newLine();
-                writer.close();
+        //checks if entry exists already and deletes if so
+        List<String> indexLines = new ArrayList<>();
+        File indexFile = new File("./git/index");
+        try (BufferedReader reader = new BufferedReader(new FileReader(indexFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!line.contains(file.getPath())) {
+                    indexLines.add(line);
+                }
             }
         }
-        catch (IOException e){
+
+        // write to index
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(indexFile))) {
+            for (String indexLine : indexLines) {
+                writer.write(indexLine);
+                writer.newLine();
+            }
+            writer.write("blob " + hashCode + " " + file.getPath());
+            writer.newLine();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -189,26 +211,34 @@ public class Git{
                 e.printStackTrace();
             }
         }
-        else{
-            File[] allFiles = file.listFiles();
-            File file_combined = new File("./combined" + file.getName());
-            if (!allFiles.equals(null)){
-                for (File child : allFiles){
-                    try {
-                        //write to objects directory
-                        BufferedWriter output = new BufferedWriter(new FileWriter(file_combined));
-                        output.write(child.getName());
-                        output.newLine();
-                        output.close();
-                    }
-                    catch (Exception e){
-                        e.printStackTrace();
+        else if (file.isDirectory()) {
+            try {
+                File[] allFiles = file.listFiles();
+                if (allFiles == null) {
+                    return null;
+                }
+                List<String> hashes = new ArrayList<>();
+                for (File child : allFiles) {
+                    String childHash = Sha1Hash(child);
+                    if (childHash != null) {
+                        hashes.add(childHash);
                     }
                 }
+                Collections.sort(hashes);
+                MessageDigest digester = MessageDigest.getInstance("SHA-1");
+                for (String hash : hashes) {
+                    digester.update(hash.getBytes());
+                }
+                byte[] sha1bytes = digester.digest();
+                BigInteger sha1data = new BigInteger(1, sha1bytes);
+                String hash = sha1data.toString(16);
+                while (hash.length() < 40) {
+                    hash = "0" + hash;
+                }
+                return hash;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            String hash = Sha1Hash(file_combined);
-            boolean bool1 = file_combined.delete();
-            return (hash);
         }
         return null;
     }
@@ -253,6 +283,183 @@ public class Git{
             if (childFile.isDirectory())
                 removeDirectory(childFile);
             childFile.delete();
+        }
+    }   
+
+
+    public String commit(String author, String message) {
+        //Gets the line from the head file
+        String parent;
+        try {
+            List<String> al = Files.readAllLines(Paths.get("./git/HEAD"));
+            if (al.isEmpty()) {
+                parent = "";
+            } else {
+                parent = al.get(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Commit failed: HEAD file issue");
+            return null;
+        }
+    
+        //creates the fileTree
+        List<String> fileTree = new ArrayList<String>();
+        try {
+            String parentFileTree="";
+            if (!parent.equals("")) {
+                for (String line : Files.readAllLines(Paths.get("./git/objects/" + parent))) {
+                    if (line.startsWith("tree:")) {
+                        parentFileTree = line.substring(6);
+                    }
+                }
+                fileTree.addAll(0, Files.readAllLines(Paths.get("./git/objects/" + parentFileTree)));
+            }
+            fileTree.addAll(0, Files.readAllLines(Paths.get("./git/index")));
+            Collections.sort(fileTree);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Commit failed: index file issue");
+            return null;
+        }
+    
+        //writes the contents of fileTree to the root tree file and gets its hash
+        File rootTree = new File("./git/objects/rootTree");
+        BufferedWriter output;
+        try {
+            output = new BufferedWriter(new FileWriter(rootTree));
+            for (int i = 0; i < fileTree.size(); i++) {
+                output.write(fileTree.get(i));
+                if (i < fileTree.size() - 1) {
+                    output.write("\n");
+                }
+            }
+            output.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        String rootTreeHash = Sha1Hash(rootTree);
+    
+        //Adds the whole file tree for this commit to the objects folder
+        File bigTreeFile = new File("./git/objects/" + rootTreeHash);
+        if (!bigTreeFile.exists()) {
+            try {
+                Files.copy(rootTree.toPath(), bigTreeFile.toPath());
+                rootTree.delete();
+            } catch (IOException e) {
+                System.out.println("bigTreeFile creation failed");
+                e.printStackTrace();
+            }
+        }
+    
+        //Creates the actual commit file
+        Date date = new Date();
+        StringBuilder commitText = new StringBuilder("");
+        commitText.append("parent: " + parent + "\n");
+        commitText.append("tree: " + rootTreeHash + "\n");
+        commitText.append("author: " + author + "\n");
+        commitText.append("date: " + date + "\n");
+        commitText.append("message: " + message);
+        MessageDigest digester;
+        String hash = "";
+        File commitFile = null;
+        try {
+            digester = MessageDigest.getInstance("SHA-1");
+            byte[] sha1bytes = digester.digest(commitText.toString().getBytes());
+            BigInteger sha1data = new BigInteger(1, sha1bytes);
+            hash = sha1data.toString(16);
+            while (hash.length() < 40) {
+                hash = "0" + hash;
+            }
+            commitFile = new File("./git/objects/" + hash);
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    
+        BufferedWriter commitWriter;
+        try {
+            commitWriter = new BufferedWriter(new FileWriter(commitFile));
+            commitWriter.write(commitText.toString());
+            commitWriter.close();
+            //updates the HEAD file
+            try (BufferedWriter headWriter = new BufferedWriter(new FileWriter("./git/HEAD"))) {
+                headWriter.write(hash);
+            }
+            //clears the index file
+            (new File("./git/index")).delete();
+            (new File("./git/index")).createNewFile();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return hash;
+    }
+    
+
+    public void checkout(String commitHash) {
+        //clear workingRepo
+        File workingRepo = new File("./workingRepo/");
+        if (workingRepo.exists()) {
+            removeDirectory(workingRepo);
+        }
+        workingRepo.mkdir();
+        //Finds commit file
+        File commitFile = new File("./git/objects/" + commitHash);
+        if (!commitFile.exists()) {
+            System.out.println("WARNING: Commit " + commitHash + " does not exist.");
+            return;
+        }
+        //finds the tree hash
+        String treeHash = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(commitFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("tree: ")) {
+                    treeHash = line.substring(6);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        //finds tree file and restores the files into workingRepo
+        File treeFile = new File("./git/objects/" + treeHash);
+        try (BufferedReader treeReader = new BufferedReader(new FileReader(treeFile))) {
+            String line;
+            while ((line = treeReader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                String type = parts[0];
+                String fileHash = parts[1];
+                String path = parts[2];
+                File objectFile = new File("./git/objects/" + fileHash);
+                File recreatePath = new File(path);
+                if (type.equals("blob")) {
+                    remakeBlob(objectFile, recreatePath);
+                } else {
+                    if (!recreatePath.exists()) {
+                        recreatePath.mkdirs();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //halpar method
+    private void remakeBlob(File source, File destination) {
+        try {
+            File parentDir = destination.getParentFile();
+            if (parentDir != null && !parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+            Files.copy(source.toPath(), destination.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- GitTester.java
+++ GitTester.java
@@ -1,18 +1,19 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.io.*;
-
 
 public class GitTester {
     public static void main (String [] args) throws IOException{
         //initialize repository
         Git repo = new Git();
-        File test = new File("./test/");
+        File test = new File("./workingRepo/test/");
 
         //check repository initialization
         File git = new File ("./git/");
         File objects = new File ("./git/objects/");
         File index = new File ("./git/index");
+        File head = new File ("./git/HEAD");
         if (git.exists())
             System.out.println ("/git directory creation success");
         else
@@ -25,15 +26,13 @@ public class GitTester {
             System.out.println ("/index  creation success");
         else
             System.out.println ("WARNING: index creation failure");
-        
-        //check Sha1Hash
-        if (repo.Sha1Hash(test).equals("9054fbe0b622c638224d50d20824d2ff6782e308"))
-            System.out.println ("Sha1Hash functioning properly");
+        if (head.exists())
+            System.out.println ("/head  creation success");
         else
-            System.out.println ("WARNING: Sha1Hash method not functioning properly (or you changed testData.txt)");
+            System.out.println ("WARNING: head creation failure");
         
         //check Hashing
-        repo.createBlobGeneral("./test/");
+        repo.stage("./workingRepo/test/");
         boolean isInIndex = false;
         String hashCode;
         //finds compressed hashcode if nessesary
@@ -67,10 +66,84 @@ public class GitTester {
         else
             System.out.println ("WARNING: Blob object creation unsuccessful");
 
+        //tests editing files:
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File("./workingRepo/test/file.txt")))) {
+            writer.write("ummmmmmmm what the sigma");
+        }
+        repo.stage("./workingRepo/test/");
+        //changes the file back to its original state:
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File("./workingRepo/test/file.txt")))) {
+            writer.write("ummmm what the sigmaejnrjpeorjweopi");
+        }
+
         //leave true if you want repository to reset at end of test
         if (false){
             repo.deleteRepository();
         }
+        //commitTest();
+        if (false){
+            repo.deleteRepository();
+        }
         
+    }
+    private static void commitTest() throws IOException {
+        Git repo = new Git();
+        repo.stage("./workingRepo/test/");
+        String commitHash = repo.commit("rizzlord", "word gng");
+        //checks if the commit file is created
+        File commitFile = new File("./git/objects/" + commitHash);
+        if (commitFile.exists()) {
+            System.out.println("Commit file creation successful");
+        } else {
+            System.out.println("WARNING: Commit file creation unsuccessful");
+        }
+        
+        //verifies the contents of the commit file
+        String commitFileText = new String(Files.readAllBytes(commitFile.toPath()));
+        if (commitFileText.contains("author: rizzlord") && commitFileText.contains("message: word gng")) {
+            System.out.println("Commit content correct");
+        } else {
+            System.out.println("WARNING: Commit content is incorrect");
+        }
+
+        //Tests second commit
+        //create new file
+        File newFile = new File("./workingRepo/newTestFile.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(newFile))) {
+            writer.write("skibidi skibidi on the wall who is the sigmaest of them all?");
+        }
+        repo.stage("./workingRepo/newTestFile.txt");
+        //second commit
+        String secondCommitHash = repo.commit("rizzlord", "second commit");
+        //verify second commit
+        File secondCommitFile = new File("./git/objects/" + secondCommitHash);
+        if (secondCommitFile.exists()) {
+            System.out.println("Second commit file creation successful");
+        } else {
+            System.out.println("WARNING: Second commit file creation unsuccessful");
+        }
+
+        //verify contents of second commit file
+        String secondCommitFileText = new String(Files.readAllBytes(secondCommitFile.toPath()));
+        if (secondCommitFileText.contains("author: rizzlord") && secondCommitFileText.contains("message: second commit")) {
+            System.out.println("Second commit content correct");
+        } else {
+            System.out.println("WARNING: Second commit content is incorrect");
+        }
+
+        
+
+        //tests checkout
+        repo.checkout(commitHash);
+        if ((new File("./workingRepo/test/")).exists()) {
+            System.out.println("Second commit file creation successful");
+        } else {
+            System.out.println("WARNING: Second commit file creation unsuccessful");
+        }
+        if(!newFile.exists()) {
+            System.out.println("New file deleted correctly");
+        } else {
+            System.out.println("WARNING: new file deleted incorrectly");
+        }
     }
 }
